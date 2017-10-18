ec84c88a380f35718f52dbaac5f8c01e75869058
Vadim Dirsha <faqproshival@yandex.ru>
2017-10-18 16:22:24 +0300

    added UI

---
+++ app/src/main/java/com/zedzul/github/hw4_12102017/ICalculator.java
@@ -0,0 +1,7 @@
+package com.zedzul.github.hw4_12102017;
+
+interface ICalculator {
+
+    String solve(String value);
+
+}
--- app/src/main/java/com/zedzul/github/hw4_12102017/MainActivity.java
+++ app/src/main/java/com/zedzul/github/hw4_12102017/MainActivity.java
@@ -2,12 +2,50 @@ package com.zedzul.github.hw4_12102017;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final ICalculator mCalculator = new Calculator();
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_user_info);
+
+        /*final View pSolveButton = findViewById(R.id.solve_button);
+        final EditText pInputEditText = (EditText) findViewById(R.id.input_edit_text);
+        final TextView pResultTextView = (TextView) findViewById(R.id.result_text_view);
+
+
+        pSolveButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(final View pView) {
+                pResultTextView.setText(mCalculator.solve(pInputEditText.getText().toString()));
+            }
+        });
+
+        pInputEditText.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(final CharSequence pCharSequence, final int pI, final int pI1, final int pI2) {
+
+            }
+
+            @Override
+            public void onTextChanged(final CharSequence pCharSequence, final int pI, final int pI1, final int pI2) {
+                pSolveButton.setEnabled(!"".equals(pCharSequence));
+            }
+
+            @Override
+            public void afterTextChanged(final Editable pEditable) {
+
+            }
+        });*/
     }
 }
